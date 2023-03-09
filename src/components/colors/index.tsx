8a0af8dfbc225d9b6b4e3340630b970a3fa47cbc
Ogechukwu Onyejekwe <ogeonyejekwejane@gmail.com>
2023-03-09 23:41:54 +0100

    delete unused files and set welcome page

--- src/components/colors/index.tsx
+++ src/components/colors/index.tsx
@@ -1,6 +1,7 @@
 export const colors ={
     white: '#ffffff',
+    white2: '#fbd8d8',
     black: '#000000',
-    primaryColor: 'green',
+    primaryColor: "#081908",
 
 }
--- src/pages/welcomePage/styles.tsx
+++ src/pages/welcomePage/styles.tsx
@@ -4,9 +4,27 @@ import { colors } from "../../components";
 export const Wrapper = styled.div`
 	width: 100%;
 	height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 	background: ${colors.primaryColor};
+
 `;
 export const Title = styled.p`
-	font-size: 30px;
+	font-size: 60px;
 	font-weight: 600;
+    color: ${colors.white2};
+    animation: slideInFromBottom 2s ease-in-out;
+	@keyframes slideInFromBottom {
+		0% {
+            margin-bottom: -50vh;
+			/* transform: translateY(50vh); */
+			opacity:0;
+		}
+		100% {
+            margin: 0px;
+			/* transform: translateY(0px); */
+			opacity:1;
+		}
+	}
 `;
